06a95532ab92b6ac342ca9ff069dd8dfc0b980bf
Juan Farias <dientuki@gmail.com>
2020-01-07 23:48:20 -0300

    added assets files

---
+++ wp-content/themes/argenbom-mk1/assets/js/front.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-new */
+import lzl from './modules/lazyload/native';
+__webpack_public_path__ = `${window.location.protocol}//${window.location.host}/dist/`;
+
+const settings_lzl = {
+  data_src: 'original',
+  elements_selector: '.lzl'
+};
+
+if ('IntersectionObserver' in window) {
+  lzl.init(settings_lzl);
+} else {
+  import(/* webpackChunkName: "lzlVanilla" */ './modules/lazyload/vanilla').then((module) => {
+      module.vanilla(settings_lzl);
+  });
+}
