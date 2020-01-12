01f22cdc9eea91a7c296785957c7b875ca933115
Juan <dientuki@gmail.com>
2020-01-12 01:28:44 -0300

    new-widget

--- wp-content/themes/argenbom-mk1/widgets/news-widget.php
+++ wp-content/themes/argenbom-mk1/widgets/news-widget.php
@@ -1,14 +1,14 @@
 <section class="widget-news">
 
   <div class="wrapper">
-    <div>
-      deco
+    <div class="widget-news__deco">
+      
     </div>
-    <div>
-      <div><?php echo $news['title']; ?></div>
-      <div><?php echo $news['text']; ?></div>
+    <div class="widget-news__text">
+      <div class="widget-news__title"><?php echo $news['title']; ?></div>
+      <div class="widget-news__desc"><?php echo $news['text']; ?></div>
     </div>
-    <div>
+    <div class="widget-news__more">
       <a class="more-info" href="<?php echo get_permalink($news['news']); ?>">info</a>
     </div>
   </div>
