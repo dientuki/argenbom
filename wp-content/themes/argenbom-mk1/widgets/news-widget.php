fffb72a9bebeb08a20a268d4b2715035ed4a9afa
Juan <dientuki@gmail.com>
2020-01-12 11:05:11 -0300

    column widget

--- wp-content/themes/argenbom-mk1/widgets/news-widget.php
+++ wp-content/themes/argenbom-mk1/widgets/news-widget.php
@@ -1,15 +1,17 @@
 <section class="widget-news">
 
   <div class="wrapper">
-    <div class="widget-news__deco">
-      
-    </div>
-    <div class="widget-news__text">
-      <div class="widget-news__title"><?php echo $news['title']; ?></div>
-      <div class="widget-news__desc"><?php echo $news['text']; ?></div>
-    </div>
-    <div class="widget-news__more">
-      <a class="more-info" href="<?php echo get_permalink($news['news']); ?>">info</a>
+    <div class="widget-news__container">
+      <div class="widget-news__deco">
+        
+      </div>
+      <div class="widget-news__text">
+        <div class="widget-news__title"><?php echo $news['title']; ?></div>
+        <div class="widget-news__desc"><?php echo $news['text']; ?></div>
+      </div>
+      <div class="widget-news__more">
+        <a class="more-info more-info-white" href="<?php echo get_permalink($news['news']); ?>">info</a>
+      </div>
     </div>
   </div>
 
