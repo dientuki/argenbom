71a4c81696d04348bffe0ddd9de2d548073da643
Juan <dientuki@gmail.com>
2020-01-12 23:16:58 -0300

    fix webpack

--- wp-content/themes/argenbom-mk1/webpack/plugins.js
+++ wp-content/themes/argenbom-mk1/webpack/plugins.js
@@ -1,7 +1,6 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const paths = require("./paths");
 const plugins = [];
 
@@ -14,6 +13,10 @@ plugins.push(new MiniCssExtractPlugin({
 }));
 
 plugins.push(new WebpackAssetsManifest());
-plugins.push(new HardSourceWebpackPlugin());
+
+if (process.env.NODE_ENV == 'development') {
+  const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+  //plugins.push(new HardSourceWebpackPlugin());
+}
 
 module.exports = plugins;
