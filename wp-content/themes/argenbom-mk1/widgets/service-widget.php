cb3c34ae3e9b3dcaed2ffd32c459827b95785a99
Juan <dientuki@gmail.com>
2020-01-12 23:59:51 -0300

    html

--- wp-content/themes/argenbom-mk1/widgets/service-widget.php
+++ wp-content/themes/argenbom-mk1/widgets/service-widget.php
@@ -3,21 +3,23 @@
 <section class="widget-service">
 
   <div class="wrapper">
-    <article class="widget-service__article">
-      <h1 class="widget-service__title uppercase"><?php echo $parent_page->post_title ?></h1>
-      <div class="widget-service__content">
-        <?php echo apply_filters( 'the_content', $parent_page->post_content ); ?>
-      </div>
-    </article>
+    <div class="widget-service__container">
+      <article class="widget-service__article">
+        <h1 class="widget-service__title uppercase bold"><?php echo $parent_page->post_title ?></h1>
+        <div class="widget-service__content">
+          <?php echo apply_filters( 'the_content', $parent_page->post_content ); ?>
+        </div>
+      </article>
 
-    <ul class="widget-service__list">
-      <?php while ( $pages->have_posts() ): $pages->the_post(); ?>
-        <li class="widget-service__list-item">
-          <a class="widget-service__list-link uppercase" href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
-          <a class="more-info more-info-green uppercase" href="<?php echo the_permalink(); ?>">info</a>
-        </li>
-      <?php endwhile; ?>
-    </ul>
+      <ul class="widget-service__list">
+        <?php while ( $pages->have_posts() ): $pages->the_post(); ?>
+          <li class="widget-service__list-item">
+            <a class="widget-service__list-link uppercase" href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
+            <a class="more-info more-info-green uppercase" href="<?php echo the_permalink(); ?>">info</a>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    </div>
   </div>
 
 </section>
