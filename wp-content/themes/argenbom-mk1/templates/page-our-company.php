99d175522abde2bd160483cd7e58f391d0418eb0
Juan <dientuki@gmail.com>
2020-02-14 18:12:17 -0300

    more css

--- wp-content/themes/argenbom-mk1/templates/page-our-company.php
+++ wp-content/themes/argenbom-mk1/templates/page-our-company.php
@@ -8,16 +8,20 @@ Template Post Type: page
   <?php get_header(); ?>
   <?php the_post() ?>
   
-  <main class="background-white">
+  <main class="background-white our-company">
     <div class="wrapper">
       <?php $fields = array('company', 'customers', 'experience', 'distributor'); ?>
-      <?php foreach($fields as $field): ?>
-        <?php $tmp = get_field_object( $field ); ?>
-        <?php if ($tmp): ?>
-          <div class="listview-products__acf-title"><?php echo $tmp['label']; ?></div>
-          <div class="listview-products__acf-value"><?php echo nl2br($tmp['value']); ?></div>
-        <?php endif; ?>
-      <?php endforeach; ?>
+      <div class="our-company-wrapper">
+        <?php foreach($fields as $field): ?>
+          <?php $tmp = get_field_object( $field ); ?>
+          <?php if ($tmp): ?>
+            <div class="our-company__item">
+              <div class="listview-products__acf-title"><?php echo $tmp['label']; ?></div>
+              <div class="listview-products__acf-value"><?php echo nl2br($tmp['value']); ?></div>
+            </div>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      </div>
 
       <?php $contact = get_field_object( 'contact' ); ?>
       <?php if ($contact): ?>
