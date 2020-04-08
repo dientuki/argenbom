7f5351bb1b7900d5e13bfe913a7bf3a20498ebac
Juan <dientuki@gmail.com>
2020-04-08 12:23:14 -0300

    change order

--- wp-content/themes/argenbom-mk1/templates/page-productos.php
+++ wp-content/themes/argenbom-mk1/templates/page-productos.php
@@ -10,7 +10,7 @@ $active = true;
 
 <main>
 
-  <?php $products = new WP_Query( array('post_type' => 'productos', 'orderby' => 'title', 'order'   => 'DESC') ) ;?>
+  <?php $products = new WP_Query( array('post_type' => 'productos', 'orderby' => 'title', 'order'   => 'DESC', 'posts_per_page' => -1) ) ;?>
 
   <?php if ( $products->have_posts() ):?>
     <div class="carrousel background-blue">
